a295a5a67710de43c86e015016f7e1345a514ef3
vinayakkankanwadi@hotmail.com <vinayakkankanwadi@hotmail.com>
2018-10-20 05:37:22 +1000

    Updates

---
+++ precache-manifest.549b881ff74e9bda4e4a5a9d4652513b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7d99752c8d47c08bca6",
+    "url": "/static/js/main.f7d99752.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f7d99752c8d47c08bca6",
+    "url": "/static/css/main.0aa2ba86.chunk.css"
+  },
+  {
+    "revision": "840b796715457a7d699da153d3a896ee",
+    "url": "/index.html"
+  }
+];
